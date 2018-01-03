4a85c2fc9d9bcac4114955875c137c80d403e56e
Djalma Araujo <djalma@close.io>
2018-01-03 18:19:29 +0000

    Move defaults to isDev

--- services/config.js
+++ services/config.js
@@ -2,15 +2,13 @@ const path = require('path')
 const Conf = require('conf')
 const isDev = require('../utils/isDev')
 
-const defaults = require('../test/fixtures/entries')
-const configParams = {
-  projectName: `wrklogger`,
-  defaults: (isDev) ? defaults : {}
-}
+const configParams = { projectName: `wrklogger` }
 
 if (isDev) {
+  const defaults = require('../test/fixtures/entries')
   const randConfig = `test_${Math.random().toString().replace('.', '_')}`
 
+  configParams.defaults = defaults
   configParams.projectName = `wrklogger${isDev ? randConfig : ''}`
   configParams.cwd = path.resolve(__dirname, `../test/tmp/${randConfig}`)
 }
