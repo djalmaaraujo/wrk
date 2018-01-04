850d7a83a64ae046adceb34a9bdf6938e148e70d
Djalma Araujo <djalma@close.io>
2018-01-04 00:19:18 +0000

    Change the ui of the token validation

--- commands/sync.js
+++ commands/sync.js
@@ -13,6 +13,22 @@ const commandList = require('../commands/entry.list')
 // Services
 const Sync = require('../services/sync')
 
+const validateToken = async (token) => {
+  const spinner = ora('Verifying your token against Github API...').start()
+
+  await Sync.setToken(token)
+
+  try {
+    const isValid = await Sync.checkToken()
+    spinner.succeed('Token verified.')
+
+    return isValid
+  } catch(err) {
+
+    spinner.fail(`${chalk.bold.red('🛑 ')} Token invalid. Github returned bad credentials status code. Provide a valid personal access token`)
+  }
+}
+
 module.exports = async (arg, opts) => {
   return inquirer.prompt([{
     type: 'confirm',
@@ -27,32 +43,15 @@ module.exports = async (arg, opts) => {
       type: 'password',
       name: 'token',
       message: "Paste your generated token here:",
-      async validate(token) {
-        const spinner = ora('Verifying your token against Github API...').start()
-
-        await Sync.setToken(token)
-
-        try {
-          const isValid = await Sync.checkToken()
-          spinner.stop()
-
-          return isValid
-        } catch(err) {
-
-          spinner.stop()
-          return `${chalk.bold.red('🛑 ')} Token invalid. Github returned bad credentials status code. Provide a valid personal access token`
-        }
-
-      }
+      validate: validateToken
     }]).then(async answers => {
       await Sync.setToken(answers.token)
 
       l.blank()
-      l.success('Github Personal token saved successfully.')
-      l.blank()
-      l.log('- All your entries will now be synced with Github Gists API')
-      l.log('- A gist with the title wrklogger.json will be added to your gists')
-      l.log('- If you wish to disable sync, type wrk sync again')
+      l.success('Github Personal token saved successfully')
+      l.log('  - All your entries will now be synced with Github Gists API')
+      l.log('  - A gist with the title wrklogger.json will be added to your gists')
+      l.log('  - If you wish to disable sync, type wrk sync again')
       l.blank()
       l.default()
     })
